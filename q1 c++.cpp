03907d79b6ac724b933ab2bc2304b4829c5a12eb
gouthamgoutham99 <123544909+gouthamgoutham99@users.noreply.github.com>
2024-03-09 09:42:58 +0530

    Add files via upload

---
+++ q1 c++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2;
+	cout<<"enter two numbers";
+	cin>>n1>>n2;
+	if(n1==n2){
+		cout<<"the given numbers are equal";
+	}
+	else{
+		cout<<"the given numbers are not equal";
+		------------------+-+------------------------------*----------------------------------------+++++++++++++++*************************************---*-----------------------
+}
+}
